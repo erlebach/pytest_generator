df14779ec3cdde2c62690cf3250761cb4c2bafd8
erlebach <gordon.erlebach@gmail.com>
2024-05-06 19:12:03 -0400

    Removed commented out lines.

--- test_generator.py
+++ test_generator.py
@@ -45,36 +45,9 @@ def create_config_dict():
     types = config.get("types", {})
     config_dict = {}
     config_dict['answer_type'] = config.get("all_tests").get("type", "float")
-    # config_dict["rel_tol"] = (
-        # config.get("types", {}).get("float", {}).get("rel_tol", 0.01)
-    # )
-    # config_dict["abs_tol"] = (
-        # config.get("types", {}).get("float", {}).get("abs_tol", 0.01)
-    # )
-    # str_choices = config.get("test_answers", {}).get("str_choices", [])
-    # config_dict["str_choices"] = config.get("types", {}).get("str_choices", [])
-    #config_dict["dict_float_choices"] = config.get("types", {}).get(
-        #"dict_float_choices", {}
-    #)
     config_dict["partial_score_frac"] = config.get("all_tests", {}).get(
         "partial_score_frac", {}
     )
-    # print(f"{config_dict['partial_score_frac']=}")
-
-    # config_dict["remove_spaces"] = config_dict.get("option_defaults", {}).get(
-        # "remove_spaces", False
-    # )
-    # config_dict["monotone_increasing"] = config_dict.get("monotone_increasing", {}).get(
-        # "monotone_increasing", False
-    # )
-
-    # config_dict["exclude_indices"] = (
-        # config.get("types", {}).get("list[string]", {}).get("exclude_indices", [])
-    # )
-    # config_dict["include_indices"] = (
-        # config.get("types", {}).get("list[string]", {}).get("include_indices", [])
-    # )
-    # config_dict["outer_key_choices"] = []  # default
 
     config_dict["student_folder_name"] = config.get("all_tests").get(
         "student_folder_name", "student_code_with_answers"
@@ -111,7 +84,7 @@ from {fixture_import_file} import *
 from function_dictionaries import *
 #import {fixture_import_file}
 import numpy as np
-from pytest_utilities import get_current_function
+from tests.pytest_utilities import get_current_function
 import yaml
 # pytest might change the python path. Make sure to import it last. 
 # import pytest
@@ -123,23 +96,7 @@ with open('type_handlers.yaml', 'r') as f:
 # ----------------------------------------------------------------------
 
 def generate_test_answers_code(questions_data, sim_type, output_file="test_answers.py"):
-    # global rel_tol, abs_tol, exclude_indices, include_indices
-    # global str_choices, dict_float_choices
-
-    # Fill in data from configuration file from the 'all_tests' section
     """
-    questions_data["student_folder_name"] = config_dict.get(
-        "student_folder_name", "student_code_with_answers"
-    )
-    questions_data["instructor_folder_name"] = config_dict.get(
-        "instructor_folder_name", "instructor_code_with_answers"
-    )
-    questions_data["i_answer_source"] = config_dict.get(
-        "i_answer_source", "instructor_file"
-    )
-    questions_data["s_answer_source"] = config_dict.get(
-        "s_answer_source", "student_file"
-    )
     """
     # Update the questions_data with the keys of config_dict that are not present in questions_data
     for k, v in config['all_tests'].items():
@@ -157,20 +114,6 @@ def generate_test_answers_code(questions_data, sim_type, output_file="test_answe
             if k not in questions_data:
                 questions_data[k] = v
 
-    """
-    config_dict["student_folder_name"] = config.get(
-        "student_folder_name", "student_code_with_answers"
-    )
-    config_dict["instructor_folder_name"] = config.get(
-        "instructor_folder_name", "instructor_code_with_answers"
-    )
-    config_dict["i_answer_source"] = config.get(
-        "instructor_answer", "instructor_code_with_answers"
-    )
-    config_dict["s_answer_source"] = config.get(
-        "student_answer", "student_code_with_answers"
-    )
-    """
     print(f"==> {questions_data=}")
 
     module_ = questions_data["module"]
@@ -252,6 +195,7 @@ def generate_test_answers_code(questions_data, sim_type, output_file="test_answe
                 test_code += f"def {function_name}():\n"
 
             test_code += f"    function_name = {function_name}\n"
+            test_code += f"    function_name = get_current_function()\n"
 
             is_fixture = (
                 fixture is not None
@@ -297,59 +241,8 @@ def generate_test_answers_code(questions_data, sim_type, output_file="test_answe
                 test_code += "    local_namespace = {}\n"
 
                 import_file = f"type_handlers['types']['{part_type}']['import']"
-                # rel_tol = part.get("rel_tol", config_dict["rel_tol"])
-                # abs_tol = part.get("abs_tol", config_dict["abs_tol"])
-                # str_choices = part.get("str_choices", config_dict["str_choices"])
-                # dict_float_choices = part.get(
-                    # "dict_float_choices", config_dict["dict_float_choices"]
-                # )
-                # monotone_increasing = part.get(
-                    # "monotone_increasing", config_dict["monotone_increasing"]
-                # )
-                # remove_spaces = part.get(
-                    # "remove_spaces",
-                    # question.get("remove_spaces", config_dict["remove_spaces"]),
-                # )
-
-                # indices to exclude from grading for list[float]
-                # Ignore index if in exclude list
-                # exclude_indices = part.get(
-                    # "exclude_indices", config_dict["exclude_indices"]
-                # )
-                # test_code += f"    exclude_indices = {exclude_indices}\n"
-                # test_code += (
-                    # f"    local_namespace['exclude_indices'] = exclude_indices\n"
-                # )
-
-                # indices to include from grading for list[float]
-                # Ignore index if not in include list
-                # include_indices = part.get(
-                    # "include_indices", config_dict["include_indices"]
-                # )
-                # test_code += f"    include_indices = {include_indices}\n"
-                # test_code += (
-                    # f"    local_namespace['include_indices'] = include_indices\n"
-                # )
-
-                # test_code += f"    rel_tol = {rel_tol}\n"
-                # test_code += f"    abs_tol = {abs_tol}\n"
 
                 test_code += add_attribute("options", options)
-                # test_code += add_attribute("str_choices", str_choices)
-                # test_code += add_attribute("monotone_increasing", monotone_increasing)
-
-                # if part_type in ["dict[str,float]", "dict[str,dict[str,float]]"]:
-                    # test_code += f"    dict_float_choices = {dict_float_choices}\n"
-                    # test_code += f"    local_namespace['dict_float_choices'] = dict_float_choices\n"
-
-                # if part_type in ["str", "dict[str,float]", "list[str]"]:
-                    # test_code += f"    remove_spaces = {remove_spaces}\n"
-                    # test_code += (
-                        # f"    local_namespace['remove_spaces'] = remove_spaces\n"
-                    # )
-
-                # test_code += f"    local_namespace['rel_tol'] = rel_tol\n"
-                # test_code += f"    local_namespace['abs_tol'] = abs_tol\n"
 
                 assertion_answer = eval(
                     f"type_handlers['types']['{part_type}']['assert_answer']"
@@ -357,10 +250,6 @@ def generate_test_answers_code(questions_data, sim_type, output_file="test_answe
                 assertion_structure = eval(
                     f"type_handlers['types']['{part_type}']['assert_structure']"
                 )  # Only difference
-                #keys = part.get(
-                    #"keys", None
-                #)  ### <<<< different: optional keys to consider (temporary)
-                # test_code += f"    keys = {keys}\n"
 
                 if eval(import_file):
                     test_code += f"    import {eval(import_file)}\n"
@@ -375,12 +264,6 @@ def generate_test_answers_code(questions_data, sim_type, output_file="test_answe
 
                 local_vars_dict = part.get("locals", None)
                 test_code += add_attribute("local_vars_dict", local_vars_dict)
-
-                # One of a finite number of choices for string type
-                # choices = part.get("choices", [])
-                # if choices == [] and (type == "string" or type == "str"):
-                    # choices = []
-                # test_code += add_attribute("choices", choices)
 
                 note = part.get("note", None)
                 if sim_type == "answers" and note is not None:
